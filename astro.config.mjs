a45a39ba91ff7fdbd8db1da577f99d8ca73f6b5a
Nodar Davituri <ndavituri@gmail.com>
2025-06-12 14:04:18 +0400

    Add Google Tag Manager script to head in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import { ion } from "starlight-ion-theme";
 import icon from "astro-icon";
+import { siteConfig } from "./site.config.js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +18,13 @@ export default defineConfig({
         dark: "./src/assets/logo-dark.svg",
         replacesTitle: true,
       },
+      head: [
+        {
+          tag: 'script',
+          content:
+            "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','" + siteConfig.gtmId + "');",
+        },
+      ],
       // social: {
       //   github: "https://github.com/Omedia/mcp-server-drupal",
       // },
---
+++ site.config.ts
@@ -0,0 +1,3 @@
+export const siteConfig = {
+    gtmId: "GTM-5CFQ58BK"
+};
